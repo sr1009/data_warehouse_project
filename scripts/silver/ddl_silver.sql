66825e2369b9e55d14a22c704ac3789f1486acc4
Shikhar Saini <shikhar16rk@gmail.com>
2026-07-24 14:25:27 +0200

    Refactor DDL script for silver tables

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -1,98 +1,98 @@
 /*
 ===============================================================================
-DDL Script: Create Silver Tables
+ddl script: create silver tables
 ===============================================================================
-Script Purpose:
-    This script creates tables in the 'silver' schema, dropping existing tables 
-    if they already exist.
-	  Run this script to re-define the DDL structure of 'bronze' Tables
+purpose:
+    sets up all tables in the 'silver' schema. existing tables are dropped 
+    first so the ddl can be cleanly re-applied whenever needed.
 ===============================================================================
 */
 
-IF OBJECT_ID('silver.crm_cust_info', 'U') IS NOT NULL
-    DROP TABLE silver.crm_cust_info;
-GO
+if object_id('silver.crm_cust_info', 'U') is not null
+    drop table silver.crm_cust_info;
+go
 
-CREATE TABLE silver.crm_cust_info (
-    cst_id             INT,
-    cst_key            NVARCHAR(50),
-    cst_firstname      NVARCHAR(50),
-    cst_lastname       NVARCHAR(50),
-    cst_marital_status NVARCHAR(50),
-    cst_gndr           NVARCHAR(50),
-    cst_create_date    DATE,
-    dwh_create_date    DATETIME2 DEFAULT GETDATE()
+create table silver.crm_cust_info (
+    cst_id             int,
+    cst_key            nvarchar(50),
+    cst_firstname      nvarchar(50),
+    cst_lastname       nvarchar(50),
+    cst_marital_status nvarchar(50),
+    cst_gndr           nvarchar(50),
+    cst_create_date    date,
+    dwh_create_date    datetime2 default getdate()
 );
-GO
+go
 
-IF OBJECT_ID('silver.crm_prd_info', 'U') IS NOT NULL
-    DROP TABLE silver.crm_prd_info;
-GO
+if object_id('silver.crm_prd_info', 'U') is not null
+    drop table silver.crm_prd_info;
+go
 
-CREATE TABLE silver.crm_prd_info (
-    prd_id          INT,
-    cat_id          NVARCHAR(50),
-    prd_key         NVARCHAR(50),
-    prd_nm          NVARCHAR(50),
-    prd_cost        INT,
-    prd_line        NVARCHAR(50),
-    prd_start_dt    DATE,
-    prd_end_dt      DATE,
-    dwh_create_date DATETIME2 DEFAULT GETDATE()
+create table silver.crm_prd_info (
+    prd_id          int,
+    cat_id          nvarchar(50),
+    prd_key         nvarchar(50),
+    prd_nm          nvarchar(50),
+    prd_cost        int,
+    prd_line        nvarchar(50),
+    prd_start_dt    date,
+    prd_end_dt      date,
+    dwh_create_date datetime2 default getdate()
 );
-GO
+go
 
-IF OBJECT_ID('silver.crm_sales_details', 'U') IS NOT NULL
-    DROP TABLE silver.crm_sales_details;
-GO
+if object_id('silver.crm_sales_details', 'U') is not null
+    drop table silver.crm_sales_details;
+go
 
-CREATE TABLE silver.crm_sales_details (
-    sls_ord_num     NVARCHAR(50),
-    sls_prd_key     NVARCHAR(50),
-    sls_cust_id     INT,
-    sls_order_dt    DATE,
-    sls_ship_dt     DATE,
-    sls_due_dt      DATE,
-    sls_sales       INT,
-    sls_quantity    INT,
-    sls_price       INT,
-    dwh_create_date DATETIME2 DEFAULT GETDATE()
+create table silver.crm_sales_details (
+    sls_ord_num     nvarchar(50),
+    sls_prd_key     nvarchar(50),
+    sls_cust_id     int,
+    sls_order_dt    date,
+    sls_ship_dt     date,
+    sls_due_dt      date,
+    sls_sales       int,
+    sls_quantity    int,
+    sls_price       int,
+    dwh_create_date datetime2 default getdate()
 );
-GO
+go
 
-IF OBJECT_ID('silver.erp_loc_a101', 'U') IS NOT NULL
-    DROP TABLE silver.erp_loc_a101;
-GO
+if object_id('silver.erp_loc_a101', 'U') is not null
+    drop table silver.erp_loc_a101;
+go
 
-CREATE TABLE silver.erp_loc_a101 (
-    cid             NVARCHAR(50),
-    cntry           NVARCHAR(50),
-    dwh_create_date DATETIME2 DEFAULT GETDATE()
+create table silver.erp_loc_a101 (
+    cid             nvarchar(50),
+    cntry           nvarchar(50),
+    dwh_create_date datetime2 default getdate()
 );
-GO
+go
 
-IF OBJECT_ID('silver.erp_cust_az12', 'U') IS NOT NULL
-    DROP TABLE silver.erp_cust_az12;
-GO
+if object_id('silver.erp_cust_az12', 'U') is not null
+    drop table silver.erp_cust_az12;
+go
 
-CREATE TABLE silver.erp_cust_az12 (
-    cid             NVARCHAR(50),
-    bdate           DATE,
-    gen             NVARCHAR(50),
-    dwh_create_date DATETIME2 DEFAULT GETDATE()
+create table silver.erp_cust_az12 (
+    cid             nvarchar(50),
+    bdate           date,
+    gen             nvarchar(50),
+    dwh_create_date datetime2 default getdate()
 );
-GO
+go
 
-IF OBJECT_ID('silver.erp_px_cat_g1v2', 'U') IS NOT NULL
-    DROP TABLE silver.erp_px_cat_g1v2;
-GO
+if object_id('silver.erp_px_cat_g1v2', 'U') is not null
+    drop table silver.erp_px_cat_g1v2;
+go
 
-CREATE TABLE silver.erp_px_cat_g1v2 (
-    id              NVARCHAR(50),
-    cat             NVARCHAR(50),
-    subcat          NVARCHAR(50),
-    maintenance     NVARCHAR(50),
-    dwh_create_date DATETIME2 DEFAULT GETDATE()
+create table silver.erp_px_cat_g1v2 (
+    id              nvarchar(50),
+    cat             nvarchar(50),
+    subcat          nvarchar(50),
+    maintenance     nvarchar(50),
+    dwh_create_date datetime2 default getdate()
 );
-GO
+go
+
 
